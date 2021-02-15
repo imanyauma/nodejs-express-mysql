821978119c1fd52b4dc2d92198564cf7b106fc2b
Yauma Imany <yauma8610@gmail.com>
2021-02-15 14:00:19 +0700

    change server.js and add Dockerfile

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const https = require('https');
+const fs = require('fs');
+const privateKey  = fs.readFileSync('/var/run/secrets/server.key', 'utf8');
+const certificate = fs.readFileSync('/var/run/secrets/server.cert', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 const app = express();
+const httpsServer = https.createServer(credentials, app);
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -16,8 +21,10 @@ app.get("/", (req, res) => {
 
 require("./app/routes/customer.routes.js")(app);
 
+httpsServer.listen(3000);
+
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
